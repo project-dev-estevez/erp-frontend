bf3b64e544cba149a58558d6ad269b76e7375f32
oskildo <podolopezospina@gmail.com>
2024-06-05 14:29:38 -0500

    product-detail

--- src/app/dashboard/warehouse/warehouse-routing.module.ts
+++ src/app/dashboard/warehouse/warehouse-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainContentWarehousePageComponent } from './pages/main-content-warehouse-page/main-content-warehouse-page.component';
+import { ProductDetailPageComponent } from './pages/product-detail-page/product-detail-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainContentWarehousePageComponent
+  },
+  {
+    path: 'product-detail/:id',
+    component: ProductDetailPageComponent
   }
 ];
 
