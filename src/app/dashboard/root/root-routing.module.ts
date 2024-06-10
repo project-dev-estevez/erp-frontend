005a17c9ab8e362a3a697e57e3f423b298b800bb
Irvin Fernandez <ifernandez.estevezjor@gmail.com>
2024-06-10 09:09:54 -0600

    Edit Profile

--- src/app/dashboard/root/root-routing.module.ts
+++ src/app/dashboard/root/root-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditUserPageComponent } from './pages/edit-user-page/edit-user-page.component';
+import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
 
 const routes: Routes = [
   {
     path: 'edit-user',
     component: EditUserPageComponent
+  },
+  {
+    path: 'edit-profile',
+    component: EditProfileComponent
   }
 ];
 
