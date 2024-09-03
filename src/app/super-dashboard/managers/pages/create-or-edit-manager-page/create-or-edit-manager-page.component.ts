c102fff11383528fbaf34353256f722224bc377a
Ruth Amaranta <ruth.rivera@estevezjor.mx>
2024-09-03 10:32:10 -0600

    Managersdos

--- src/app/super-dashboard/managers/pages/create-or-edit-manager-page/create-or-edit-manager-page.component.ts
+++ src/app/super-dashboard/managers/pages/create-or-edit-manager-page/create-or-edit-manager-page.component.ts
@@ -1,34 +1,109 @@
-import { Component, OnInit } from '@angular/core';
-import { Department } from 'src/app/super-dashboard/departments/interfaces';
-import { DepartmentsService } from 'src/app/super-dashboard/departments/services/departments.service';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ManagersService } from '../../services/managers.service';
+import { SweetAlertService } from "@shared/services/sweet-alert.service";
+import { Router, ActivatedRoute } from '@angular/router';
+import { CreateManagerDto } from '../../interfaces/create-manager.dto';
+import { UpdateManagerDto } from '../../interfaces/update-manager.dto';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-create-or-edit-manager-page',
   templateUrl: './create-or-edit-manager-page.component.html',
   styleUrl: './create-or-edit-manager-page.component.scss'
 })
-export class CreateOrEditManagerPageComponent implements OnInit {
 
-  public departments: Department[] = [];
+export class CreateOrEditManagerPageComponent implements OnInit{
+
+  private managerId: string = '';
+
+  public managerForm: FormGroup = this.fb.group({
+    fullName: ['', [ Validators.required ]],
+    email: ['', [ Validators.required]],
+    password: ['', [ Validators.required, Validators.minLength(7) ]]
+  });
 
   constructor(
-    private departmenstsService: DepartmentsService
-  ) {}
-
-
+    private ManagersService: ManagersService,
+    private sweetAlert: SweetAlertService,
+    private fb: FormBuilder,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ){}
 
   ngOnInit(): void {
+    this.checkIsUpdate();
+  }
 
-    this.departmenstsService.getAllDepartments().subscribe(
+  private createManager(){
+
+    const CreateManagerDto: CreateManagerDto = this.managerForm.value;
+
+    this.ManagersService.createManager( CreateManagerDto ).subscribe(
       response => {
-        this.departments = response.results;
+        const name = response.name;
+        this.sweetAlert.presentSuccess( `Gerente ${name} creado con éxito` );
+        this.redirectToList();
       },
       errorResponse => {
-        console.log(errorResponse);
+        this.sweetAlert.presentError('Creando el gerente');
       }
     );
+  }
 
+  private updateManager(){
+    const UpdateManagerDto: UpdateManagerDto = this.managerForm.value;
 
+    this.ManagersService.updateManagerById(this.managerId, UpdateManagerDto).subscribe(
+      response =>{
+        this.sweetAlert.presentSuccess(`Manager ${response.fullName} actualizado con éxito`);
+        this.redirectToList();
+      },
+      errorResponse => {
+        this.sweetAlert.presentError('Actualizando gerente');
+      }
+    );
+  }
+
+  private checkIsUpdate(){
+    const route = this.router.url;
+    if( !route.includes('edit'))return;
+
+    this.activatedRoute.params.pipe(
+      switchMap( ({id})=>{
+        this.managerId = id;
+        return this.ManagersService.getManagerById( id );
+      } )
+    ).subscribe(
+      response => {
+        if(!response)return this.router.navigateByUrl('/');
+
+        this.managerForm.reset({
+          fullName: response.fullName,
+          email: response.email,
+          password: response.password
+        });
+        return;
+      },
+      errorResponse => {
+        this.sweetAlert.presentError('Obteniendo gerente por Id');
+        return this.router.navigateByUrl('/');
+      }
+    )
+
+  }
+
+  onSubmit(){
+    if( this.managerId ){
+      this.updateManager();
+      return;
+    }
+
+    this.createManager();
+  }
+
+  redirectToList(){
+    this.router.navigateByUrl('/super-dashboard/managers');
   }
 
 }
--- src/app/super-dashboard/managers/pages/list-managers-page/list-managers-page.component.ts
+++ src/app/super-dashboard/managers/pages/list-managers-page/list-managers-page.component.ts
@@ -1,10 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Manager } from '../../interfaces/manager-entity';
+import { TableColumn } from '@shared/interfaces/table-column';
+import { TableConfig } from '@shared/interfaces/table-config';
+import { SweetAlertService } from '@shared/services/sweet-alert.service';
+import { Router } from '@angular/router';
+import { TableAction } from '@shared/interfaces/table-action';
+import { TABLE_ACTION } from '@shared/enums/table-action.enum';
+import { ManagersService } from '../../services/managers.service';
 
 @Component({
   selector: 'app-list-managers-page',
   templateUrl: './list-managers-page.component.html',
   styleUrl: './list-managers-page.component.scss'
 })
-export class ListManagersPageComponent {
+export class ListManagersPageComponent implements OnInit {
 
+  public dataList: Manager[] = [];
+
+  public tableColumns: TableColumn[] = [
+    { label: 'Nombre', def:'name', dataKey:'fullName' },
+  ];
+
+  public tableConfig: TableConfig = {
+    showActions: true,
+    totalItemsPagination: 0
+  };
+
+  constructor(
+    private managersService: ManagersService,
+    private sweetAlert: SweetAlertService,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    this.loadDataList();
+  }
+
+  private loadDataList(){
+    this.managersService.getAllManagers().subscribe(
+      response => {
+        this.dataList = response.results;
+        this.tableConfig.totalItemsPagination = response.total;
+      },
+      errorResponse => {
+        console.log(errorResponse);
+      }
+    );
+  }
+
+  onClickInCreate(){
+    this.router.navigate(['/super-dashboard/managers/new-manager']);
+  }
+
+  onTableAction( tableAction: TableAction){
+    const id = tableAction.row.id;
+
+    switch ( tableAction.action ){
+      case TABLE_ACTION.EDIT:
+        this.router.navigate([`/super-dashboard/managers/edit/${id}`]);
+        break;
+
+        case TABLE_ACTION.SHOW:
+          this.router.navigate([`/super-dashboard/managers/show/${id}`]);
+          break;
+
+          case TABLE_ACTION.DELETE:
+            this.deleteManager( id );
+            break;
+
+            default:
+              break;
+    }
+  }
+
+  async deleteManager( id: string ){
+
+    const { isConfirmed } = await this.sweetAlert.presentDelete(`${id}`);
+    if( !isConfirmed ) return;
+
+     this.managersService.deleteManagerById( id ).subscribe(
+      response => {
+        this.sweetAlert.presentSuccess( 'Gerente elimidado' );
+        this.loadDataList();
+      },
+      errorResponse => {
+        console.log(errorResponse);
+      }
+    )
+
+}
 }
